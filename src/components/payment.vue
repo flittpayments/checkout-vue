23dd727970f5b45e4191e99ede53ef95c5444d03
Михайло Голубовський <lamerumiata@gmail.com>
2018-10-23 11:26:02 +0300

    feat add banks logos (WAL-138)

--- src/components/payment.vue
+++ src/components/payment.vue
@@ -305,6 +305,9 @@
             deepMerge(this.params, params, notSet.params)
           }
         })
+        this.$root.$on('resize', () => {
+          this.resizeWindow()
+        })
       },
       autoFocus: function () {
         if (this.errors.count()) {
