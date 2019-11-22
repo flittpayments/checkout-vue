351c55b890b35d7d217af4be210f22df5112c9f9
Михайло Голубовський <lamerumiata@gmail.com>
2019-11-22 15:47:16 +0200

    refactor(component): payment-fields

--- src/components.js
+++ src/components.js
@@ -1,4 +1,3 @@
-import FPaymentFields from '@/components/payment-fields'
 import FPaymentMethod from '@/components/payment-method'
 import FMethods from '@/components/methods'
 import FFastAccess from '@/components/fast-access'
@@ -14,7 +13,6 @@ import FError from '@/components/error'
 import FPopover from '@/components/popover'
 
 const components = {
-  FPaymentFields,
   FPaymentMethod,
   FMethods,
   FFastAccess,
