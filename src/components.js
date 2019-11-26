02178e02ecdd291340020509436010c5fc23ce6a
Михайло Голубовський <lamerumiata@gmail.com>
2019-11-26 13:31:40 +0200

    refactor(component): offer

--- src/components.js
+++ src/components.js
@@ -1,4 +1,3 @@
-import FOffer from '@/components/offer'
 import FRegular from '@/components/regular'
 import FWalletPayButton from '@/components/wallet-pay-button'
 import FSvg from '@/components/svg'
@@ -7,7 +6,6 @@ import FError from '@/components/error'
 import FPopover from '@/components/popover'
 
 const components = {
-  FOffer,
   FRegular,
   FWalletPayButton,
   FSvg,
