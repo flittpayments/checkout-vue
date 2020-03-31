a16e1c7a47705028e191cc9e4c6c7cd266a9c94a
Михайло Голубовський <lamerumiata@gmail.com>
2020-03-31 12:49:26 +0300

    refactor: project structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,7 +2,7 @@ import $checkout from 'js-sdk/dist/esm/checkout'
 import store from '@/store'
 import config from '@/config/config'
 import Schema from 'async-validator'
-import { i18n } from '@/i18n'
+import i18n from '@/i18n/index'
 
 export const api = $checkout('Api')
 let cache = {}
