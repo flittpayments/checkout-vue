62097e05e8ee82d8631350e4a5bb309e90b68423
Михайло Голубовський <lamerumiata@gmail.com>
2020-06-17 14:27:16 +0300

    refactor(component): tooltip (WAL-258)

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@ import FSvg from '@/components/svg'
 import FMask from '@/components/mask'
 import FPopover from '@/components/popover'
 import FModal from '@/components/modal'
-import FTooltip from '@/components/tooltip/tooltip.vue'
+import FTooltipBase from '@/components/tooltip/tooltip-base'
+import FTooltipError from '@/components/tooltip/tooltip-error'
+import FTooltipDefault from '@/components/tooltip/tooltip-default'
 import FDropdown from '@/components/dropdown'
 import InputText from '@/components/input-text'
 import InputAmount from '@/components/input-amount'
@@ -19,7 +21,9 @@ const components = {
   FMask,
   FPopover,
   FModal,
-  FTooltip,
+  FTooltipBase,
+  FTooltipError,
+  FTooltipDefault,
   FDropdown,
   InputText,
   InputAmount,
