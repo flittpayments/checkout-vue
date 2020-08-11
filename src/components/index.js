3931a1131f3ce0aa197b603a44cd28ea6005c8fb
Михайло Голубовський <lamerumiata@gmail.com>
2020-08-11 10:24:12 +0300

    feat(component): add scrollbar-vertical (WAL-258)

--- src/components/index.js
+++ src/components/index.js
@@ -24,6 +24,7 @@ import FFormItem from '@/components/form/item'
 import FFormGroup from '@/components/form/group'
 import FFormItemInput from '@/components/form/item/input'
 import FFormItemDate from '@/components/form/item/date'
+import FScrollbarVertical from '@/components/scrollbar-vertical'
 
 const components = {
   FButtonPayWallet,
@@ -52,6 +53,7 @@ const components = {
   FFormGroup,
   FFormItemInput,
   FFormItemDate,
+  FScrollbarVertical,
 }
 
 const install = Vue => {
