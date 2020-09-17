35277646b11880c11eb04b6fa82825cca4cf9ec6
Михайло Голубовський <lamerumiata@gmail.com>
2020-09-17 17:46:01 +0300

    fix: flag form isSubmit

--- src/config/options-default.js
+++ src/config/options-default.js
@@ -83,7 +83,7 @@ const options = {
   css: configCss.default,
   loading: false,
   cards: [],
-  submit: false,
+  isSubmit: false,
   submited: false,
   read_only: false,
   need_verify_code: false,
