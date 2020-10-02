66dd35bd1949bba48cc895cc48c7be89f91cff86
Михайло Голубовський <lamerumiata@gmail.com>
2020-10-02 23:14:04 +0300

    refactor: user config validation

--- src/store/index.js
+++ src/store/index.js
@@ -114,7 +114,7 @@ export default {
     }
   },
   validate(options) {
-    new Schema(config).validate(options, errors => {
+    new Schema(config).validate({ config: options }, errors => {
       if (!errors) return
       this.setError(errors)
     })
