56196ecee0d362193e89d50fb6a5630523b723ca
Михайло Голубовський <lamerumiata@gmail.com>
2020-10-15 22:30:34 +0300

    chore(component): add form item checkbox

--- src/components/index.js
+++ src/components/index.js
@@ -24,6 +24,7 @@ import FFormItem from '@/components/form/item'
 import FFormGroup from '@/components/form/group'
 import FFormItemInput from '@/components/form/item/input'
 import FFormItemDate from '@/components/form/item/date'
+import FFormItemCheckbox from '@/components/form/item/checkbox'
 import FScrollbarVertical from '@/components/scrollbar-vertical'
 import FButtonLink from '@/components/button/button-link'
 import FButton from '@/components/button/button'
@@ -58,6 +59,7 @@ const components = {
   FFormGroup,
   FFormItemInput,
   FFormItemDate,
+  FFormItemCheckbox,
   FScrollbarVertical,
   FButtonLink,
   FButton,
