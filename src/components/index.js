ab3efed6bf98369d5414fcb92836685f363e7711
Михайло Голубовський <lamerumiata@gmail.com>
2021-01-21 14:49:37 +0200

    feat(component): add placeholder (WAL-318)

--- src/components/index.js
+++ src/components/index.js
@@ -32,6 +32,7 @@ import FBank from '@/components/bank'
 import FSecurity from '@/components/security'
 import FPreloader from '@/components/preloader'
 import FAmount from '@/components/base/amount'
+import FPlaceholder from '@/components/base/placeholder'
 
 const components = {
   FButtonPayWallet,
@@ -68,6 +69,7 @@ const components = {
   FSecurity,
   FPreloader,
   FAmount,
+  FPlaceholder,
 }
 
 const install = Vue => {
