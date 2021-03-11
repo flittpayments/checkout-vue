701980981bbe4d99384c3a988e895de5eaac4ae0
Михайло Голубовський <lamerumiata@gmail.com>
2021-03-11 16:49:25 +0200

    refactor(chunk): css-vars-ponyfill

--- src/import.js
+++ src/import.js
@@ -74,3 +74,8 @@ export const loadCheckout = () =>
   import(
     /* webpackChunkName: "ipsp-js-sdk" */ 'js-sdk/dist/esm/checkout'
   ).then(module => module.default)
+
+export const loadCssVars = () =>
+  import(/* webpackChunkName: "css-vars-ponyfill" */ 'css-vars-ponyfill').then(
+    module => module.default
+  )
