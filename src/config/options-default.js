201645b338bb8d2e73d1d3fcf9716b86de817a83
Михайло Голубовський <lamerumiata@gmail.com>
2021-07-07 12:38:09 +0300

    feat: display discount (WAL-356)

--- src/config/options-default.js
+++ src/config/options-default.js
@@ -50,7 +50,6 @@ const config = {
   params: {
     merchant_id: 1396424, // 900024 dev, 1396424 prod
     amount: 0,
-    amount_with_fee: 0,
     fee: 0, // %
     currency: '',
     recurring: 'n',
@@ -108,6 +107,9 @@ const config = {
   can_make_payment: '',
   has_fields: false,
   need_validate_card: true,
+  amount_with_fee: 0,
+  actual_amount: 0,
+  card_type_fee: 0,
 }
 
 locales.forEach(function (locale) {
