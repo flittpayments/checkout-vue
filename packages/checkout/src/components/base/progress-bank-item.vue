89c612cf5098e8c0b65565b5bef4a0090e303620
Mykhailo Holubovskyi <mgolubovsky@flitt.com>
2025-07-25 16:53:47 +0300

    refactor(component): progress-bank-item (GWAL-52)

---
+++ packages/checkout/src/components/base/progress-bank-item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="f-bank-item">
+    <f-progress class="f-mr-12" size="48x48" />
+    <div class="f-bank-item-wrapper">
+      <f-progress size="20" />
+    </div>
+  </div>
+</template>
+
+<script>
+import FProgress from '@/components/base/progress'
+
+export default {
+  components: { FProgress },
+}
+</script>
