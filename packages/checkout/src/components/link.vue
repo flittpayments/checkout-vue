6292580970c33828ea2c76978412f871e1bef8b8
Mykhailo Holubovskyi <mgolubovsky@flitt.com>
2026-04-21 18:53:58 +0300

    refactor(component): link

--- packages/checkout/src/components/link.vue
+++ packages/checkout/src/components/link.vue
@@ -6,35 +6,52 @@
 
 <script>
 import FButtonUnstyled from '@/components/button/button-unstyled'
+import { makeProp } from '@/utils/props'
+import { PROP_TYPE_STRING } from '@/constants/props'
 
 export default {
   components: {
     FButtonUnstyled,
   },
+  props: {
+    href: makeProp(PROP_TYPE_STRING),
+    size: makeProp(PROP_TYPE_STRING, undefined, value =>
+      ['14'].includes(value)
+    ),
+  },
   computed: {
     tag() {
-      return this.$attrs.href ? 'a' : 'f-button-unstyled'
+      return this.href ? 'a' : 'f-button-unstyled'
     },
     attrs() {
       return {
         ...this.$attrs,
-        class: this.$style.wrapper,
+        href: this.href,
+        target: this.href ? '_blank' : undefined,
+        class: this.className,
+        rel: this.href ? 'noopener noreferrer' : undefined,
       }
+    },
+    className() {
+      return [this.$style.style, this.$style[`s_${this.size}`]]
     },
   },
 }
 </script>
 
 <style lang="scss" module>
-.wrapper {
+.style {
   font-weight: 400;
-  font-size: px-to-rem(14px);
-  line-height: px-to-rem(20px);
-  color: $main;
+  color: #788fff;
   text-decoration: underline;
 
   &:hover {
     text-decoration: none;
   }
+}
+
+.s_14 {
+  font-size: px-to-rem(14px);
+  line-height: px-to-rem(20px);
 }
 </style>
