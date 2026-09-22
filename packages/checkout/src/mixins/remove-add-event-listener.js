a47080e7fa4d6998b2ca01908c0aa610204db38a
Mykhailo Holubovskyi <mgolubovsky@flitt.com>
2026-09-22 15:38:29 +0300

    feat: input migration to CSS Modules (GWAL-53)

--- packages/checkout/src/mixins/remove-add-event-listener.js
+++
@@ -1,9 +0,0 @@
-// @vue/component
-export const removeAddEventListenerMixin = {
-  mounted() {
-    this.$nextTick().then(() => {
-      const input = this.$refs.input.$el || this.$refs.input
-      input.addEventListener = () => {}
-    })
-  },
-}
